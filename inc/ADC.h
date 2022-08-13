0f2aa23701e76491f62d43a15650122af0b65ade
abao <talent518@yeah.net>
2022-08-14 02:09:54 +0800

    调整3.3V电压误差。

--- inc/ADC.h
+++ inc/ADC.h
@@ -6,9 +6,9 @@
 void ADC1_Init(void);
 u16 ADC1_GET(u8 ch);
 
-#define ADC_GetTemp() ((1.43f - ((float) ADC1_GET(ADC_Channel_TempSensor) * 3.3f / 4095.0f)) / 0.0043f + 25.0f)
-#define ADC_GetVref() ((float) ADC1_GET(ADC_Channel_Vrefint) * 3.3f / 4095.0f)
-#define ADC_GetVoltage1() ((float) ADC1_GET(ADC_Channel_8) * 3.3f / 4095.0f)
-#define ADC_GetVoltage2() ((float) ADC1_GET(ADC_Channel_9) * 3.3f / 4095.0f)
+#define ADC_GetTemp() ((1.43f - ((float) ADC1_GET(ADC_Channel_TempSensor) * 3.35f / 4095.0f)) / 0.0043f + 25.0f)
+#define ADC_GetVref() ((float) ADC1_GET(ADC_Channel_Vrefint) * 3.35f / 4095.0f)
+#define ADC_GetVoltage1() ((float) ADC1_GET(ADC_Channel_8) * 3.35f / 4095.0f)
+#define ADC_GetVoltage2() ((float) ADC1_GET(ADC_Channel_9) * 3.35f / 4095.0f)
 
 #endif
